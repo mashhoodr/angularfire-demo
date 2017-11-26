665793f085c1d411f36b5f75c24444bc73dedecc
Mashhood Rastgar <mashhoodr@gmail.com>
2017-11-26 11:58:59 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  items: Observable<any[]>;
+
+  constructor(db: AngularFirestore) {
+    this.items = db.collection('users').valueChanges();
+
+    this.items.subscribe(items => {
+      console.log(items);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
-
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AppRoutingModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
